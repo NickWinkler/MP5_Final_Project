095341dbfb7e2f3614faf57b8ef3741a0d4f6a63
NickWinkler <ngw4@illinois.edu>
2019-04-19 19:52:13 -0500

    Current version

--- app/src/main/java/com/example/mp5/AllLabsFragment.java
+++ app/src/main/java/com/example/mp5/AllLabsFragment.java
@@ -10,9 +10,24 @@ import android.view.ViewGroup;
 
 public class AllLabsFragment extends Fragment {
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_all_labs, container, false);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
     }
 }
