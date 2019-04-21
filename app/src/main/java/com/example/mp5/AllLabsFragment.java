97541282f598959c16e9f0a7136054943e0924a5
NickWinkler <ngw4@illinois.edu>
2019-04-20 23:44:36 -0500

    All labs list being implemented

--- app/src/main/java/com/example/mp5/AllLabsFragment.java
+++ app/src/main/java/com/example/mp5/AllLabsFragment.java
@@ -4,21 +4,41 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class AllLabsFragment extends Fragment {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_all_labs, container, false);
+        View view = inflater.inflate(R.layout.fragment_all_labs, container,
+                false);
+
+        RecyclerView recyclerView = (RecyclerView) view
+                .findViewById(R.id.recycler_view);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(
+                getActivity().getBaseContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setHasFixedSize(true);
+
+        adapter = new LabListAdapter();
+
+        recyclerView.setAdapter(adapter);
+        return view;
     }
 
     @Override
