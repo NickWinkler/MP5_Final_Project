bc4e0711d30e860853c7de60bb33739b3ea09f42
NickWinkler <ngw4@illinois.edu>
2019-04-26 14:31:12 -0500

    Distance matrix api working

--- app/src/main/java/com/example/mp5/DistancesAsyncTask.java
+++ app/src/main/java/com/example/mp5/DistancesAsyncTask.java
@@ -9,6 +9,14 @@ import android.support.v7.app.AppCompatActivity;
 import com.example.lib.LocationData;
 import com.google.android.gms.location.LocationServices;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class DistancesAsyncTask extends AsyncTask<String, String, String> {
     private final String API_KEY = "AIzaSyDkJezIq93KVGDpJarXLIqsxs2-xg7nL9c";
     private Context mainContext;
@@ -27,14 +35,41 @@ public class DistancesAsyncTask extends AsyncTask<String, String, String> {
             double latitude = location.getLatitude();
             System.out.println("Getting distances in background");
 
-            String url = "https://maps.googleapis.com/maps/api/distancematrix/json?";
-            url = url + "origins:" + latitude + "," + longitude;
+            String url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&mode=walking";
+            url = url + "&origins=" + latitude + "," + longitude;
             url = url + "&destinations=";
             for (String[] lab : LocationData.locationData) {
                 url = url + lab[2] + "," + lab[3] + "|";
             }
             url = url.substring(0, url.length() - 1);
+            url = url + "&key=" + API_KEY;
             System.out.println(url);
+
+
+            System.out.println("Getting JSON data");
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            URL webUrl = new URL(url);
+            connection = (HttpURLConnection) webUrl.openConnection();
+            connection.connect();
+
+
+            InputStream stream = connection.getInputStream();
+
+            reader = new BufferedReader(new InputStreamReader(stream));
+
+            StringBuffer buffer = new StringBuffer();
+            String line = "";
+
+            while ((line = reader.readLine()) != null) {
+                buffer.append(line+"\n");
+                //Log.d("Response: ", "> " + line);   //here u ll get whole response...... :-)
+
+            }
+
+            return buffer.toString();
+
         } catch (SecurityException e) {
             e.printStackTrace();
         }catch (Exception e) {
@@ -42,5 +77,11 @@ public class DistancesAsyncTask extends AsyncTask<String, String, String> {
         }
         System.out.println("Do in background returning");
         return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+
     }
 }
